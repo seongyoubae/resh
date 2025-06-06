ad21395deb6133254999c7262bfecd7354ffb77b
seongyou <seongyou517@snu.ac.kr>
2025-06-06 16:50:52 +0900

    학습 환경 수정
    15x15, 330강판

--- reshulffing/data.py
+++ reshulffing/data.py
@@ -119,8 +119,8 @@ def main():
     rows = ['A', 'B']
     df_plan, from_piles_reshuffle, mapping_from_pile_to_x, piles_all, x_max = generate_reshuffle_plan(
         rows,
-        n_from_piles_reshuffle=20,
-        n_to_piles_reshuffle=20,
+        n_from_piles_reshuffle=15,
+        n_to_piles_reshuffle=15,
         n_plates_reshuffle=22,
         safety_margin=0
     )
